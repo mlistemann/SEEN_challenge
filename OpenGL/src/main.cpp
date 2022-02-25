1dec451101ddfcd43c38f2c165aea7beed3638b3
mlistemann <marc.listemann@yahoo.de>
2022-02-25 16:10:55 +0100

    Use projection matrices

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -70,13 +70,13 @@ int main(void)
 	float xTranslate = 0.0f;
 	float increment = 0.05f;
 
+	float aspectRatio = 1920.f / 1080.f;
 	// RENDER LOOP
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
-		renderer.Clear();
-		
+		renderer.Clear();		
 
 		{
 			// DRAW FIRST QUAD
@@ -84,10 +84,13 @@ int main(void)
 			model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.0f));
 			glm::mat4 view = glm::mat4(1.0f);
 			view = glm::translate(view, glm::vec3(xTranslate, 0.6f, 0.0f));
+			glm::mat4 projection = glm::mat4(1.0f);
+			projection = glm::ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f);
 
 			shader.use();
 			shader.setMat4("model", model);
 			shader.setMat4("view", view);
+			shader.setMat4("projection", projection);
 
 			shader.setVec3("u_Color", glm::vec3(0.0, 0.0, 1.0));
 
@@ -100,10 +103,13 @@ int main(void)
 			model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.0f));
 			glm::mat4 view = glm::mat4(1.0f);
 			view = glm::translate(view, glm::vec3(-xTranslate, -0.6f, 0.0f));
+			glm::mat4 projection = glm::mat4(1.0f);
+			projection = glm::ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f);
 
 			shader.use();
 			shader.setMat4("model", model);
 			shader.setMat4("view", view);
+			shader.setMat4("projection", projection);
 
 			shader.setVec3("u_Color", glm::vec3(1.0, 0.0, 0.0));
 
@@ -111,9 +117,9 @@ int main(void)
 
 		}
 
-		if (xTranslate > 1.0f)
+		if (xTranslate > (aspectRatio * 1.0f))
 			increment -= 0.05f;
-		else if (xTranslate < -1.0f)
+		else if (xTranslate < (aspectRatio * -1.0f))
 			increment += 0.05f;
 
 		xTranslate += increment;
